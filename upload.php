a4b4edfe5209a03e2be6bf112a69fc93ea98b4fa
Kilian <kiliansillerosalvatierra@gmail.com>
2019-03-26 09:22:07 +0100

    Ocr con version legacy comentada

--- upload.php
+++ upload.php
@@ -18,8 +18,22 @@ if(isset($_FILES['image'])){
         //He tenido que ponerle la ruta donde esta el ejecutable por que aun cogiendo el path en la consola desde php no va
         ->executable($path)
         ->lang("spa")
+        //->psm(12)
         ->run();
 
+
+        //version Legacy (con esta si que van los patrones, pero reconoce peor y mas lento)
+        // $resultado = (new TesseractOCR("images/$file_name"))
+        // ->executable($path)
+        // ->tessdataDir('tessdata')
+        // ->lang("spa")
+        // ->oem(0)
+        // ->userPatterns('patrones/patterns.txt')
+        // ->config('load_system_dawg', false)
+        // ->config('load_freq_dawg', false)
+        // ->psm(1)
+        // ->run();
+        
     //mostramos el resultado
     echo "<br><h3>OCR after reading</h3><br><pre>$resultado</pre>";
 
